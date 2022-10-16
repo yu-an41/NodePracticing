b5ba8f1515432d16b0397fdefbf29250da245b21
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-10-16 14:09:57 +0800

    add server via express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log(456);
+const express = require('express');
+
+// 建立web server物件
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`<h2>你好！</h2>`);
+})
+
+app.listen(3000, ()=> {
+    console.log('server started');
+});
